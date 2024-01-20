63a7617ca2ac377ac7000e1fb6e5669f0a82eead
fukumamire <rinka_karyou@hotmail.com>
2024-01-20 14:29:44 +0900

    ０－６　バリデーション設定　フォームリクエストの設定

--- src/app/Http/Requests/ContactRequest.php
+++ src/app/Http/Requests/ContactRequest.php
@@ -13,7 +13,7 @@ class ContactRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -27,7 +27,6 @@ class ContactRequest extends FormRequest
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255'],
             'tel' => ['required', 'numeric', 'digits_between:10,11'],
-
 
         ];
     }
